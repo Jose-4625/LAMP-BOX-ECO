1b638393dd5fdde9da3d216768b83b38a288e632
jat4625 <jatorr98@gmail.com>
2019-11-15 13:22:10 -0600

    init

--- workSpace/LAMP.py
+++ workSpace/LAMP.py
@@ -1,30 +1,23 @@
 from MicroWebSrv2 import *
 from time import sleep
 
+#create webroute
 @WebRoute(GET, '/')
 def RequestTest(microWebSrv2, request):
-    content = "./www/index.html.gz"
-
-    request.Response.SetHeader('Accept-Encoding','gzip, deflate')
     request.Response.SetHeader('Content-Encoding','gzip')
-    request.Response.SetHeader('Content-Type','application/x-gzip')
-    request.Response.ReturnFile(content)
+    request.Response.SetHeader('Content-Type','text/css')
 
-@WebRoute(GET, '/temp')
-def RequestTest2(microWebSrv2, request):
-    request.Response.ReturnOkJSON({
-        'ClientAddr': request.UserAddress,
-        'Accept': request.Accept,
-        'UserAgent': request.UserAgent
-    })
-
-
+    content = open("./www/index.html", 'r').read()
+    request.Response.ReturnOk(content)
+    print(request.Response._headers)
+#Start Server
 mws = MicroWebSrv2()
 mws.SetEmbeddedConfig()
+mws.AddMimeType('.gz','application/x-gzip')
 mws.BindAddress = ('127.0.0.1',3000)
 mws.StartManaged()
 
-
+#keep server running
 try :
     while True :
         sleep(60)
