c3ec71e5a55483fc7e2a4bdf85b8cdd19bbed397
Jose Torres <jatorr98@gmail.com>
2020-01-19 15:29:02 -0600

    Added key functions to _machine.py

--- workSpace/_machine.py
+++ workSpace/_machine.py
@@ -2,12 +2,13 @@ class Pin:
     def __init__(self, v):
         self.pin = v
 class PWM:
-    def __init__(self, pin):
+    def __init__(self, pin, freq=0, duty=0):
         self.pin = pin
-        self.freq = 0
-        self.duty = 0
+        self.freq = freq
+        self.duty = duty
     def freq(x):
         self.freq = x
     def duty(x):
         self.duty = x
-     
+    def deinit():
+        return 0
